992f7d1c06f401d62a9545abf10f746e9c172aa5
Justin PC <justin@pedowitz.net>
2018-05-21 20:41:56 -0400

    First Commit
    Commiting the 3D Map Project

---
+++ AU 3D Map Fixed/Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+	public Camera cam;
+	void Update()
+	{
+
+		if (!isLocalPlayer) {
+			cam.enabled = false;
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal")*0.4f;
+		var z = Input.GetAxis("Vertical")*0.4f;
+
+		transform.Translate(x, 0, z);
+	}
+
+	public override void OnStartLocalPlayer()
+	{	
+		Vector3 playerPosition = this.transform.position;
+		playerPosition.y = 0.0f;
+		this.transform.position = playerPosition;
+
+		//GetComponent<MeshRenderer>().material.color = Color.red;
+
+
+			
+	}
+		
+}
